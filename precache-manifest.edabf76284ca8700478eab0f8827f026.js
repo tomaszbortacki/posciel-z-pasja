2f1ddc439d1986d687d62a2f27506e7b3b3c4068
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2020-09-25 00:04:17 +0200

    Updates

---
+++ precache-manifest.edabf76284ca8700478eab0f8827f026.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb185bbe1215f6f2af0ebd92ad80a9a",
+    "url": "/posciel-z-pasja/index.html"
+  },
+  {
+    "revision": "5e0204527f73df3f2329",
+    "url": "/posciel-z-pasja/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d9c78817343e797eb4ea",
+    "url": "/posciel-z-pasja/static/css/main.cbb4c650.chunk.css"
+  },
+  {
+    "revision": "5e0204527f73df3f2329",
+    "url": "/posciel-z-pasja/static/js/2.d141c2e6.chunk.js"
+  },
+  {
+    "revision": "d38e447b1f06cb9b62eb8f2d87918c9a",
+    "url": "/posciel-z-pasja/static/js/2.d141c2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c78817343e797eb4ea",
+    "url": "/posciel-z-pasja/static/js/main.82bdd994.chunk.js"
+  },
+  {
+    "revision": "aad60f6c3639478bb0fd",
+    "url": "/posciel-z-pasja/static/js/runtime-main.6317491c.js"
+  },
+  {
+    "revision": "e881c64be9cd9a689b19e4ea700c4107",
+    "url": "/posciel-z-pasja/static/media/logo.e881c64b.svg"
+  },
+  {
+    "revision": "8c231b15eafdc708c98913d1dfb6f0d1",
+    "url": "/posciel-z-pasja/static/media/mail.8c231b15.svg"
+  },
+  {
+    "revision": "e6ba86c0527e24a111552fea2df9fc11",
+    "url": "/posciel-z-pasja/static/media/phone.e6ba86c0.svg"
+  }
+]);
