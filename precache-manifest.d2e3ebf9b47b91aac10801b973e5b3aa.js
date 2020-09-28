840847adb5161ce24bc08148a940b2b7e1b5e0da
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2020-09-28 09:56:00 +0200

    Updates

---
+++ precache-manifest.d2e3ebf9b47b91aac10801b973e5b3aa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de2141917d6ba0bb3cabb504bb89893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5433fe06da91120f80f",
+    "url": "/static/css/2.fce0075c.chunk.css"
+  },
+  {
+    "revision": "d81d302ad07125c46b7a",
+    "url": "/static/css/main.ff9ca525.chunk.css"
+  },
+  {
+    "revision": "e5433fe06da91120f80f",
+    "url": "/static/js/2.8c269d42.chunk.js"
+  },
+  {
+    "revision": "d38e447b1f06cb9b62eb8f2d87918c9a",
+    "url": "/static/js/2.8c269d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81d302ad07125c46b7a",
+    "url": "/static/js/main.d985c87d.chunk.js"
+  },
+  {
+    "revision": "d4a7164f2420b3222429",
+    "url": "/static/js/runtime-main.8760b465.js"
+  },
+  {
+    "revision": "e881c64be9cd9a689b19e4ea700c4107",
+    "url": "/static/media/logo.e881c64b.svg"
+  },
+  {
+    "revision": "8c231b15eafdc708c98913d1dfb6f0d1",
+    "url": "/static/media/mail.8c231b15.svg"
+  },
+  {
+    "revision": "e6ba86c0527e24a111552fea2df9fc11",
+    "url": "/static/media/phone.e6ba86c0.svg"
+  },
+  {
+    "revision": "92521a1228199123882e9f8bc0f6d09d",
+    "url": "/static/media/search.92521a12.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/posciel-z-pasja/precache-manifest.a8ed2df87b8b2fdf89e26b445f15de47.js"
+  "/precache-manifest.d2e3ebf9b47b91aac10801b973e5b3aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/posciel-z-pasja/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
