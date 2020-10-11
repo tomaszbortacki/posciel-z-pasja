e7200511b56ec8f8ce300121b250bd976a4cee90
Tomasz Bortacki <tomasz.bortacki99@gmail.com>
2020-10-11 12:46:52 +0200

    Slider changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,7 +10,7 @@ const Home = ({ pages, socials, categories, copy }) => {
     arrows: false,
     dots: true,
     infinite: true,
-    speed: 1000,
+    speed: 1200,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
